ef25ebb93ef5a5efc3afc017c90d4d4b42dffba0
Vignesh <sallagirivignesh@gmail.com>
2026-09-08 17:29:24 +0530

    Hu Descrepnacy added

--- VS Mart Backend/VS Mart Backend/Features/HUDiscrepancy/IHUDiscrepancyService.cs
+++ VS Mart Backend/VS Mart Backend/Features/HUDiscrepancy/IHUDiscrepancyService.cs
@@ -7,5 +7,6 @@ namespace VS_Mart_Backend.Features.HUDiscrepancy
     {
         Task<VendorWiseHUDiscrepancyResponse> GetVendorHUDiscrepancyDataAsync(VendorHUDiscrepancyRequest request);
         Task<ApiResponse> StoreandUserData(StoreUserRequest req);
+        Task<ApiResponse> StoreandUserDataCache(StoreUserRequest req);
     }
 }
