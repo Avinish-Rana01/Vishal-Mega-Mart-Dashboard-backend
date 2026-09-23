e6685afa85007ca97f6c24951ed00209173c8dfe
Vignesh <sallagirivignesh@gmail.com>
2026-09-23 17:11:36 +0530

    Development done of GetWHEncodingDetailsAsync and GetHUSummaryDetails.

---
+++ VS Mart Backend/VS Mart Backend/Features/Dashboard/DCEncoding/IWHEncodingDetails.cs
@@ -0,0 +1,7 @@
+﻿namespace VS_Mart_Backend.Features.Dashboard.DCEncoding
+{
+    public interface IWHEncodingDetails
+    {
+        Task<WHEncodingResponse> GetWHEncodingDetailsAsync(WHEncodingRequest request);
+    }
+}
---
+++ VS Mart Backend/VS Mart Backend/Features/Dashboard/DCEncoding/WHEncodingDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VS_Mart_Backend.Features.Dashboard.DCEncoding
+{
+    [ApiController]
+    public class WHEncodingDetailsController : ControllerBase
+    {
+        private readonly IWHEncodingDetails _whEncodingDetails;
+
+        public WHEncodingDetailsController(IWHEncodingDetails whEncodingDetails)
+        {
+            _whEncodingDetails = whEncodingDetails;
+        }
+
+        [HttpGet("GetWHEncodingDetails")]
+        public async Task<IActionResult> GetWHEncodingDetails([FromQuery] WHEncodingRequest request)
+        {
+            try
+            {
+                var result = await _whEncodingDetails.GetWHEncodingDetailsAsync(request);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new
+                    {
+                        message = ex.Message
+                    });
+            }
+        }
+    }
+}
